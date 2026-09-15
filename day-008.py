cf520367263af7448ab178350a0efeb0637dcdba
jayrebel123 <jayrebel859@gmail.com>
2026-09-15 06:19:55 +0530

    Add Day 8 conditional logic exercise

---
+++ day-008.py
@@ -0,0 +1,25 @@
+# if statement
+'''age = int(input("enter your age is :"))
+if age >= 18:
+    print("your age is 18")
+else:
+    print("your age is below 18")'''
+
+#
+temperature = int(input("Enter your temperature :"))
+
+if temperature >= 39:
+    print("Wealth alert: today heat is high !")
+elif temperature >= 25:
+    print("wealth alert: today heat is meduim !")
+elif temperature >= 10:
+    print("wealth alert : today heat is low !")
+else:
+    print("wealth alert : today wealth is nice !")
+
+
+sunny = True
+if sunny:
+    print("get sunnyglass")
+else:
+    print("no suunyglass")
